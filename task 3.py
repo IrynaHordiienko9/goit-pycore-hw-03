a4044badeb2a2838f53883f79cc37d9ecc731a4d
Iryna Hordiienko <irynagordiyenko9@gmail.com>
2025-10-17 15:57:20 +0300

    task 3.py

---
+++ task 3.py
@@ -0,0 +1,34 @@
+import re
+
+def normalize_phone(phone_number):
+   
+    cleaned = re.sub(r'[^\d+]', '', phone_number.strip())
+    
+    if cleaned.startswith('+'):
+        
+        if cleaned.startswith('+380'):
+            return cleaned
+    
+        elif cleaned.startswith('+38'):
+            return cleaned
+        
+        else:
+            return cleaned
+    
+    elif cleaned.startswith('380'):
+        return '+' + cleaned
+    
+    else:
+        return '+38' + cleaned
+
+
+numbers = [
+    "   +38(050)123-32-34",
+    "   0503451234",
+    "(050)8889900",
+    "38050-111-22-22",
+    "38050 111 22 11   ",
+]
+
+for num in numbers:
+    print(normalize_phone(num))
